6ec4214162b14a13dd21d8132230f0fa872a25d4
Quirozdev <luisdevv232@gmail.com>
2024-03-13 21:21:49 -0700

    completed until 7.3

---
+++ using_structs/src/main.rs
@@ -0,0 +1,78 @@
+#[derive(Debug, Copy, Clone)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+    fn set_width(&mut self, width: u32) {
+        self.width = width;
+    }
+    fn max(self, other: Rectangle) -> Rectangle {
+        Rectangle {
+            width: self.width.max(other.width),
+            height: self.height.max(other.height),
+        }
+    }
+    fn set_to_max(&mut self, other: Rectangle) {
+        let max = self.max(other);
+        *self = max;
+    }
+    // "static", no requiere una instancia, no es un metodo como los anteriores
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    println!("rect1 is: {:#?}", rect1);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(3);
+
+    // syntactic sugar
+    let mut r = Rectangle {
+        width: 1,
+        height: 2,
+    };
+    let area1 = r.area();
+    let area2 = Rectangle::area(&r);
+    assert_eq!(area1, area2);
+
+    r.set_width(2);
+    Rectangle::set_width(&mut r, 2);
+}
+
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
